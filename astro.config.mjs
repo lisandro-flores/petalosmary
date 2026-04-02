17bfa1c3c7271ee48f455773e68c16c5dca9e55f
Lisandro LSoft <lisandro@lsoft.me>
2026-04-01 21:00:22 -0600

    Publicación de mejoras integrales y cambio a modo servidor para Docker deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://petalosmary.ngicode.com',
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [sitemap()],
 });
