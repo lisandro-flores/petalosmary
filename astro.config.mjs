0e1a8d199e1e649ab4f6c18e79fb85c2a4f46ffa
Lisandro <lisandro@example.com>
2026-06-04 21:36:53 -0600

    chore: configure output and adapter dynamically based on Netlify environment variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import node from '@astrojs/node';
 
+const isNetlify = process.env.NETLIFY === 'true';
+
 export default defineConfig({
   site: 'https://petalosmary.ngicode.com',
-  output: 'server',
-  adapter: node({
+  output: isNetlify ? 'static' : 'server',
+  adapter: isNetlify ? undefined : node({
     mode: 'standalone'
   }),
   integrations: [sitemap()],
 });
+
